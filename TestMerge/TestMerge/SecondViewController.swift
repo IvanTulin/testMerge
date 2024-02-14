57370187cd3cea80c768964d84921598a7ad9a3a
Natalia_Poftalnaya <poftnataly@gmail.com>
2024-02-14 21:21:53 +0300

    init2

--- TestMerge/TestMerge/SecondViewController.swift
+++ TestMerge/TestMerge/SecondViewController.swift
@@ -8,21 +8,11 @@
 import UIKit
 
 class SecondViewController: UINavigationController {
-    
-    var name = "Name"
-    var lastName = "Last name"
-    
-    private lazy var label: UILabel = {
-        let label = UILabel(frame: CGRect(x: 50, y: 150, width: 100, height: 20))
-        label.text = name + lastName
-        label.textColor = .white
-        return label
-    }()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .magenta
-        view.addSubview(label)
+
 
     }
 }
