8c9ed3d9ed8ab9eaadf90a4c135860632b28d49c
IvanTulin <tulin.v@bk.ru>
2024-02-14 21:01:50 +0300

    Creat new branch

---
+++ TestMerge/TestMerge/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  TestMerge
+//
+//  Created by Ivan Tulin on 14.02.2024.
+//
+
+import UIKit
+
+class SecondViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
